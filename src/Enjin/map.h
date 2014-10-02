eec849506711b3a5a89b3a8d83717e0100d3713d
LordFluffyPants <LordFluffyPants@users.noreply.github.com>
2014-10-02 10:49:57 -0400

    Create map.h

---
+++ src/Enjin/map.h
@@ -0,0 +1,29 @@
+/*
+ * map.h
+ *
+ *  Created on: Oct 1, 2014
+ *  Author: Jake Hayhurst
+ *  Version: 0.0
+ */
+
+#ifndef MAP_H_
+#define MAP_H_
+
+using namespace std;
+
+class map{
+
+public:
+	map();
+	map(char[][]);
+
+
+
+private:
+
+
+};
+
+
+
+#endif /* MAP_H_ */
