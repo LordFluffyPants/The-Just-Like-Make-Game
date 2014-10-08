b60e8a5ab5c419236912c531c2cb3c45cf4d7b4a
LordFluffyPants <rixmaddi@gmail.com>
2014-10-08 16:04:46 -0400

    added the unit header file

---
+++ src/Enjin/unit.h
@@ -0,0 +1,48 @@
+/*
+ * unit.h
+ *
+ *  Created on: Oct 6, 2014
+ *      Author: jake
+ */
+
+#ifndef UNIT_H_
+#define UNIT_H_
+#import <string>
+
+using namespace std;
+
+class unit {
+
+public:
+
+	unit(string name);
+	string getName();
+	int getHealth();
+	void setHealth(int);
+	int getMovementMax();
+	void setMovementMax(int);
+	double getAttackValue();
+	int getArmor();
+	void updateArmor();
+	bool getFlying();
+	string getRace();
+
+
+
+private:
+	string name;
+	int health;
+	int movementMax;
+	double attackValue;
+	int armor;
+	bool flying;
+	int additionalArmor;
+	string race;
+	void updateAttackValue();
+
+
+};
+
+
+
+#endif /* UNIT_H_ */
