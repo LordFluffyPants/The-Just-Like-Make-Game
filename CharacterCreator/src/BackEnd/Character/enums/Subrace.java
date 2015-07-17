a1627496cb9fabb787e8a8feffa822f706ae2618
LordFluffyPants <rixmaddi@gmail.com>
2015-07-16 23:30:59 -0400

    added subrace enum

--- CharacterCreator/src/BackEnd/Character/enums/Subrace.java
+++ CharacterCreator/src/BackEnd/Character/enums/Subrace.java
@@ -1,8 +1,27 @@
 package BackEnd.Character.enums;
 
 /**
- * Created by Jake on 6/27/2015.
+ * Created by Jake on 7/16/2015.
  */
 public enum Subrace {
+    DWARF,
+    GNOME,
+    STONE_SKIN,
+    FEL_TOUCHED,
+    AETHERIAL,
+    HIGHBORNE,
+    SORCS,
+    PORCS,
+    WORKS,
+    NYMPHS,
+    AURAI,
+    WILDLINGS,
+    LIZZARD_FOLK,
+    KOMODOS,
+    DRAGONKIN,
+    CRYSTAL_GOBLINS,
+    HOBOGOBO,
+    GEO_GOBLINS;
+
 
 }
